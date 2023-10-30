d872c1a833385a58b9802e945e86423fc4c4660b
 <96326525+mutasim77@users.noreply.github.com>
2023-10-30 18:00:23 +0600

    Styling done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { AiOutlineSearch } from "react-icons/ai";
 import Card from "./components/Card";
 import Globe from "./components/Globe";
 import { data } from "@/data";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -16,39 +18,43 @@ const Home = () => {
       <p className="text-center mt-5 text-xl">
         Below, you&apos;ll find a list of fantastic things related to the React ecosystem
       </p>
-      <ul className="flex items-center gap-4 mt-7">
-        {data.map((item) => (
-          <Card
-            key={item.id}
-            title={item.title}
-            size="px-5 py-2"
-          />
-        ))}
-      </ul>
 
-      <div className="my-6">
-        <input type="text" />
+      <div className="relative my-5 bg-gradient-to-br from-[#fff] to-[#adadad] rounded-md">
+        <ul className="flex overflow-x-auto scrollbar-hide space-x-4 p-4 hide-scroll-bar">
+          {data.map((item) => (
+            <li key={item.id} className="whitespace-nowrap h-10 py-1 flex justify-center items-center px-5 rounded-md text-black text-sm text-center shadow-md bg-white cursor-pointer">
+              <Link href={`#${item.title}`}>{item.title}</Link>
+            </li>
+          ))}
+          <div className="absolute left-[-20px] w-20 h-full top-0 bg-gradient-to-r from-[#090911e6] to-[#7272721a]"></div>
+          <div className="absolute right-0 w-20 h-full  top-0 bg-gradient-to-l from-[#090911e6] to-[#7272721a]"></div>
+        </ul>
+      </div>
+
+
+      <div className="my-6 relative w-1/2 m-auto">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-2xl">
+          <AiOutlineSearch className="w-5 h-5 text-black" />
+        </div>
+        <input type="text" className="search-input" placeholder="Search" />
+        <button className="search-input-button">Search</button>
       </div>
 
       {data.map((item) => (
-        <div key={item.id}>
-          <h1 className="text-center text-2xl" id={item.title}>React {item.title}</h1>
-
-          <ul className="flex flex-wrap justify-center items-center gap-2">
+        <div key={item.id} className="flex flex-col gap-7 mt-7">
+          <h1 className="h-[64px] w-1" id={item.title}></h1>
+          <h1 className="text-center text-5xl">React {item.title}</h1>
+          <div className="flex flex-wrap justify-center items-center gap-2 ">
             {item.links.map((item) => (
               <Card
                 key={item.id}
-                title={item.name}
+                name={item.name}
                 link={item.link}
-                size="w-[200px] h-[150px]"
               />
             ))}
-          </ul>
-
+          </div>
         </div>
-
       ))}
-      {/* <h1 className="mt-9" id="State Management">HELLOO</h1> */}
     </>
   )
 }
