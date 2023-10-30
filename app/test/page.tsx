efc1a2ea50713292ea247f18fe824ef97a7014a2
 <96326525+mutasim77@users.noreply.github.com>
2023-10-30 18:00:50 +0600

    Added: Testing Page - only for testing

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import { data } from '@/data'
+
+export default function page() {
+    return (
+        <>
+            <h1 className='text-5xl'>Hello, I am testing YOU!</h1>
+        </>
+    )
+}
