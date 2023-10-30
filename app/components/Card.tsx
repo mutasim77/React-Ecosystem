dfff4087026e903e1d775f8d0b1d18d86343242a
 <96326525+mutasim77@users.noreply.github.com>
2023-10-30 17:59:01 +0600

    Modified: Card Components

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,31 +1,16 @@
 import Link from "next/link";
 
-const Card = ({
-    title,
-    size,
-    link
-}: {
-    title: string,
-    size: string,
-    link?: string
+const Card = ({ name, link }: {
+    name: string,
+    link: string
 }) => {
     return (
-        <Link
-            href={`${link ? `${link}` : `#${title}`}`}
-            target={`${link ? '_blank' : '_self'}`}
-        >
-            <li
-                className={`${size} border border-[#ccc] 
-                    rounded-md text-black font-bold 
-                    bg-gradient-to-br from-[#fff] to-[#adadad]
-                    flex items-center justify-center
-                    text-center
-                `}
-            >
-                {title}
-            </li>
+        <Link href={link} target="_blank" className="card">
+            {name}
         </Link>
     )
 }
 
 export default Card;
+
+// bg-gradient-to-br from-[#fff] to-[#adadad]
