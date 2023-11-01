96024b1565f7b1a55b4ec6c17a2dc8a72eaf5b44
 <96326525+mutasim77@users.noreply.github.com>
2023-11-01 07:16:20 +0600

    🔘 Updated: Data changed to client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
-import { AiOutlineSearch } from "react-icons/ai";
-import Card from "./components/Card";
 import Globe from "./components/Globe";
 import { data } from "@/data";
 import Link from "next/link";
+import Header from "./components/Header";
+import Data from "./components/Data";
 
 const Home = () => {
   return (
     <>
       <div className="z-10 w-full px-5 xl:px-0 sm:mt-[10%]">
-        <h1
-          className="animate-fade-up bg-gradient-to-br from-[#fff] to-[#adadad] bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
-        >
-          Discover React&apos;s Full Universe
-        </h1>
+        <Header />
         <Globe />
       </div>
       <p className="text-center mt-5 text-xl">
@@ -22,7 +18,7 @@ const Home = () => {
       <div className="relative my-5 bg-gradient-to-br from-[#fff] to-[#adadad] rounded-md">
         <ul className="flex overflow-x-auto scrollbar-hide space-x-4 p-4 hide-scroll-bar">
           {data.map((item) => (
-            <li key={item.id} className="whitespace-nowrap h-10 py-1 flex justify-center items-center px-5 rounded-md text-black text-sm text-center shadow-md bg-white cursor-pointer">
+            <li key={item.id} className="whitespace-nowrap h-10 py-1 flex justify-center items-center px-5 rounded-md text-black text-sm text-center shadow-md bg-white cursor-pointer hover:scale-[1.03] transition-all">
               <Link href={`#${item.title}`}>{item.title}</Link>
             </li>
           ))}
@@ -31,30 +27,7 @@ const Home = () => {
         </ul>
       </div>
 
-
-      <div className="my-6 relative w-1/2 m-auto">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-2xl">
-          <AiOutlineSearch className="w-5 h-5 text-black" />
-        </div>
-        <input type="text" className="search-input" placeholder="Search" />
-        <button className="search-input-button">Search</button>
-      </div>
-
-      {data.map((item) => (
-        <div key={item.id} className="flex flex-col gap-7 mt-7">
-          <h1 className="h-[64px] w-1" id={item.title}></h1>
-          <h1 className="text-center text-5xl">React {item.title}</h1>
-          <div className="flex flex-wrap justify-center items-center gap-2 ">
-            {item.links.map((item) => (
-              <Card
-                key={item.id}
-                name={item.name}
-                link={item.link}
-              />
-            ))}
-          </div>
-        </div>
-      ))}
+      <Data />
     </>
   )
 }
