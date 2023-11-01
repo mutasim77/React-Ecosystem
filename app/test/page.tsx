be1436cd98249f1b89fa3ce8a31b747b61914a2c
 <96326525+mutasim77@users.noreply.github.com>
2023-11-01 07:20:09 +0600

    Deleted: Unnecessary files

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { data } from '@/data'
-
-export default function page() {
-    return (
-        <>
-            <h1 className='text-5xl'>Hello, I am testing YOU!</h1>
-        </>
-    )
-}
